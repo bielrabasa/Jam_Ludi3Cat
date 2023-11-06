150c1cad30a42a775b457f034c7a74bba7af750c
Roger <rogersalas2000@gmail.com>
2023-11-06 18:55:19 +0100

    Dark mode

--- Assets/Partida/ColorsSelect.cs
+++ Assets/Partida/ColorsSelect.cs
@@ -9,21 +9,22 @@ public class ColorsSelect : MonoBehaviour
     public Color correct = new Color(0.2745f, 0.8235f, 0.2745f, 1);
     public Color possible = new Color(0.9215f, 0.6274f, 0, 1);
 
-    public List<GameObject> clickedButtons;
+    public Color notPress;
 
+    public List<GameObject> clickedButtons;
+    public List<GameObject> notclickedButtons;
+    
     public void DaltonicsOn()
     {
         Toggle toggle = this.GetComponentInParent<Toggle>();
 
         if (toggle.isOn)
         {
-            notappears = new Color(0.145f, 0.145f, 0.145f, 0.6078f);
             correct = new Color(1f, 0.8549f, 0.149f, 1);
             possible = new Color(0.50588f, 0.8317f, 0.98f, 1);
         }
         else
         {
-            notappears = new Color(0.145f, 0.145f, 0.145f, 0.6078f);
             correct = new Color(0.2745f, 0.8235f, 0.2745f, 1);
             possible = new Color(0.9215f, 0.6274f, 0, 1);
         }
@@ -33,7 +34,27 @@ public class ColorsSelect : MonoBehaviour
         foreach(GameObject obj in clickedButtons)
         {
             vKB.ChangeColorKeyBoard(obj);
+        }
+    }
 
+    public void DarkOn(GameObject backgrounKB)
+    {
+        Toggle toggle = this.GetComponentInParent<Toggle>();
+
+        if (toggle.isOn)
+        {
+            notappears = new Color(1f, 1f, 1f, 1f);
+            backgrounKB.GetComponent<Image>().color = new Color(0,0,0);
+        }
+        else
+        {
+            notappears = new Color(0f, 0f, 0f, 1f);
+            backgrounKB.GetComponent<Image>().color = new Color(1, 1, 1);
+        }
+
+        foreach (GameObject obj in notclickedButtons)
+        {
+            obj.GetComponentInChildren<Text>().color = notappears;
         }
     }
 }
