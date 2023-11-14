87d0a97e9c57ee8e736904ee0c3183cb00cd90cd
Roger Salas <rogersalas2000@gmail.com>
2023-11-14 09:12:10 +0100

    no opacity

--- Assets/Partida/ColorsSelect.cs
+++ Assets/Partida/ColorsSelect.cs
@@ -9,11 +9,11 @@ public class ColorsSelect : MonoBehaviour
     Color background = new Color(1, 1, 1);                          //Keyboard Background lightmode
     Color background_dark = new Color(0, 0, 0);                     //Keyboard Background darkmode
 
-    Color background_game_light = new Color(0.78f, 0.78f, 0.78f);   //Game Background lightmode
-    Color background_game_dark = new Color(0.27f, 0.27f, 0.27f);    //Game Background darkmode
+    Color background_game_light = new Color(0.78f, 0.78f, 1f);   //Game Background lightmode
+    Color background_game_dark = new Color(0.27f, 0.27f, 1f);    //Game Background darkmode
 
     //Letters
-    Color none = new Color(0.5f, 0.5f, 0.5f, 0.75f);                //Keyboard non-clicked letters
+    Color none = new Color(0.5f, 0.5f, 0.5f, 1f);                //Keyboard non-clicked letters
 
     Color text = new Color(0f, 0f, 0f, 1f);                         //Keyboard font color
     Color text_dark = new Color(1f, 1f, 1f, 1f);                    //Keyboard font color darkmode
@@ -29,11 +29,11 @@ public class ColorsSelect : MonoBehaviour
 
     Color selected = new Color(0,0,0);                              //Frase letter selected
     Color not_selected = new Color(1,1,1);                          //Frase letter non selected
-    Color similar_selected = new Color(0.5f, 0.5f, 0.5f);           //Frase letter same symbol as selected
+    Color similar_selected = new Color(0.5f, 0.5f, 1f);           //Frase letter same symbol as selected
 
     Color selected_dark = new Color(1, 1, 1);                       //Frase letter selected darkmode
     Color not_selected_dark = new Color(0, 0, 0);                   //Frase letter non selected darkmode
-    Color similar_selected_dark = new Color(0.5f, 0.5f, 0.5f);      //Frase letter same symbol as selected darkmode
+    Color similar_selected_dark = new Color(0.5f, 0.5f, 1f);      //Frase letter same symbol as selected darkmode
 
     Color text_selected = new Color(0, 0, 0);                       //Frase letter non selected
     Color text_selected_dark = new Color(1f, 1f, 1f);               //Frase letter same symbol as selected
